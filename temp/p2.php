26edd054899b9a8ce01f250fc6c67884f7c984a6
Sameesh-S <sameesh.fisat@gmail.com>
2015-10-27 11:04:25 +0530

    first success

--- temp/p2.php
+++ temp/p2.php
@@ -3,8 +3,13 @@ echo "projrctdxx works"."<br>";
 //echo $data;
 $html=file_get_contents("http://www.amazon.ca/gp/product/B003EKNMAI/ref=br_asw_pdt-5?pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=desktop-6&pf_rd_r=145EBCFR6HFMH1RVN6PR&pf_rd_t=36701&pf_rd_p=2068471842&pf_rd_i=desktop");
 
-preg_match_all('/priceblock_ourprice"[\s*]class="(.*)">V</',$html,$posts);
-print_r($desired);
+preg_match_all('/<span id="priceblock_ourprice" class=".*?">(.*?)<\/span>/s',
+    $html,
+    $posts, // will contain the article data
+    PREG_SET_ORDER // formats data into an array of posts
+);
 
-echo $data;
+echo $posts[0];
+
+//print_r($posts);
 ?>
