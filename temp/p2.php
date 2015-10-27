3b2facada3f530e05d1adbb0d2a05d85c371e2cc
Sameesh-S <sameesh.fisat@gmail.com>
2015-10-27 11:12:10 +0530

    updated

--- temp/p2.php
+++ temp/p2.php
@@ -1,6 +1,5 @@
 <?php
-echo "projrctdxx works"."<br>";
-//echo $data;
+echo "project works"."<br>";
 $html=file_get_contents("http://www.amazon.ca/gp/product/B003EKNMAI/ref=br_asw_pdt-5?pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=desktop-6&pf_rd_r=145EBCFR6HFMH1RVN6PR&pf_rd_t=36701&pf_rd_p=2068471842&pf_rd_i=desktop");
 
 preg_match_all('/<span id="priceblock_ourprice" class=".*?">(.*?)<\/span>/s',
@@ -9,7 +8,5 @@ preg_match_all('/<span id="priceblock_ourprice" class=".*?">(.*?)<\/span>/s',
     PREG_SET_ORDER // formats data into an array of posts
 );
 
-echo $posts[0];
-
-//print_r($posts);
+echo $posts[0][0];
 ?>
