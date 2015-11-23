bead09a5f58b831a8a2672817df6e7039b100705
Sameesh <sameesh.fisat@gmail.com>
2015-11-22 23:59:24 -0500

    Update tagstrip.php

--- temp/tagstrip.php
+++ temp/tagstrip.php
@@ -1,10 +1,9 @@
 <?php
-$html=file_get_contents("http://www.amazon.ca/gp/product/B00SGS7ZH4/ref=s9_acss_bw_hsb_LaptopsS_s2_n?pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=merchandised-search-2&pf_rd_r=1S72BA42K5E9DADAAC62&pf_rd_t=101&pf_rd_p=2253690442&pf_rd_i=677252011");
-
+//$html=file_get_contents("http://www.amazon.ca/gp/product/B00SGS7ZH4/ref=s9_acss_bw_hsb_LaptopsS_s2_n?pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=merchandised-search-2&pf_rd_r=1S72BA42K5E9DADAAC62&pf_rd_t=101&pf_rd_p=2253690442&pf_rd_i=677252011");
 //$var1=preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $html);
 //$var1=preg_replace('/<style\b[^>]*>(.*?)<\/style>/is', "", $var1);
 //$var1=preg_replace('/<meta\b[^>]*>(.?)<\/meta>/is', "", $var1);
-
+$var1=file_get_contents($_POST["url"]);
 function strip_html_tags( $text )
 {
     $text = preg_replace(
@@ -36,6 +35,5 @@ function strip_html_tags( $text )
         $text );
     return strip_tags( $text );
 }
-
 echo strip_html_tags( $var1 );
 ?>
